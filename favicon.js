dd377e793c624ad68f8455ba8d2472163a09ad1f
Legends <78807240+xlegends@users.noreply.github.com>
2022-05-17 10:43:05 -0500

    Update favicon.js

--- favicon.js
+++ favicon.js
@@ -1,7 +1,4 @@
 if (localStorage.getItem("checked")) document.querySelector("title").innerHTML = "Google"
   
 document.querySelector("link[rel=icon]").href =((location.href.slice(-1) == "/") && (!location.href.match("\/gxmehub\/$"))? ".." : ".") + "/" + (localStorage.getItem("checked") ? "google.png" : "favicon.ico")
-     if(self != top) { 
-  document.querySelector("#please").style.display = "block"
-  document.querySelector("#content").style.display = "none"
- }
+ 
