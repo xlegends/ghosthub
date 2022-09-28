c238797ef9c5a3925ac48adf2d483b2c14a35236
Legends <78807240+xlegends@users.noreply.github.com>
2022-09-28 08:10:25 -0500

    Update gxmes.js

--- gxmes.js
+++ gxmes.js
@@ -1,6 +1,5 @@
 const elemsrendered = () => {
   document.querySelectorAll("h2").forEach(el => el.addEventListener("click",function() {
-    alert("e")
   location.href = "../play/?" + this.textContent
 }))
 
